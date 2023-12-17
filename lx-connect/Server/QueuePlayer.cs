e5647be203770b6c4baaf262c790150f7629be20
Samir Azzam <samir.azzam.official@gmail.com>
2023-12-17 17:26:56 +0200

    Connect Queue Implementation
    Basic implementation of ConnectQueue.
    Next Steps:
    - Add priority
    - Add auto priority to Admins
    - Improve queue deferral message

---
+++ lx-connect/Server/QueuePlayer.cs
@@ -0,0 +1,21 @@
+﻿using CitizenFX.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lx_connect.Server
+{
+    internal class QueuePlayer
+    {
+        public Player Player { get; set; }
+        public DateTime JoinedOn { get; set; }
+        public bool CanJoin { get; set; }
+
+        public QueuePlayer(Player player, DateTime joinedOn, bool canJoin)
+        {
+            Player = player;
+            JoinedOn = joinedOn;
+            CanJoin = canJoin;
+        }
+    }
+}
--- lx-connect/Server/ServerMain.cs
+++ lx-connect/Server/ServerMain.cs
@@ -2,6 +2,7 @@
 using lx_connect.Server.Manager;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 
@@ -11,40 +12,130 @@ namespace lx_connect.Server
     {
         private readonly string _languageConst;
         private readonly int _queueRefreshRate;
+        private readonly int _maxPlayers;
         private readonly Dictionary<string, string> _config;
         private readonly Dictionary<string, string> _language;
+        private List<QueuePlayer> waitingList = new List<QueuePlayer>();
 
         public ServerMain()
         {
+            _config = new Dictionary<string, string>();
             _config = ConfigManager.LoadConfig();
             _queueRefreshRate = int.Parse(_config["QueueRefreshRate"]);
             _languageConst = _config["Language"];
+            _maxPlayers = int.Parse(_config["MaxPlayerCount"]);
             _language = LanguageManager.LoadLanguage(_languageConst);
 
             EventHandlers["playerConnecting"] += new Action<Player, string, dynamic, dynamic>(OnPlayerConnecting);
+            EventHandlers["playerDropped"] += new Action<Player, string>(OnPlayerDropped);
+
+            Task.Run(async () => await ProcessQueue());
+        }
+
+        private async Task ProcessQueue()
+        {
+            while (true)
+            {
+                if (waitingList.Any())
+                {
+                    if (Players.Count() < _maxPlayers)
+                    {
+                        waitingList[0].CanJoin = true;
+                    } 
+                }
+
+                await Delay(10000);
+            }
+        }
+
+        private void OnPlayerDropped(Player player, string reason)
+        {
+            var queuePlayer = waitingList.FirstOrDefault(p => p.Player.Handle == player.Handle);
+            if (queuePlayer != null)
+                waitingList.Remove(queuePlayer);
         }
 
         private async void OnPlayerConnecting([FromSource] Player player, string playerName, dynamic setKickReason, dynamic deferrals)
         {
-
-
-            deferrals.defer();
-
-            deferrals.update(string.Format(_language["WelcomeMessage"],playerName));
-            await Delay(3500);
-
-            Dictionary<string, string> userIdentifiers = GetUserIdentifiers(player);
-
-            bool IsValid = await ValidateIdentifiersAsync(player,userIdentifiers, deferrals);
-
-            if(IsValid)
+            try
             {
-                deferrals.update(_language["ConnectingMessage"]);
-                await Delay(_queueRefreshRate + 3250);
-                deferrals.done();
+                deferrals.defer();
+
+                deferrals.update(string.Format(_language["WelcomeMessage"], playerName));
+                await Delay(3500);
+
+                Dictionary<string, string> userIdentifiers = GetUserIdentifiers(player);
+                bool IsValid = await ValidateIdentifiersAsync(player, userIdentifiers, deferrals);
+
+                if (IsValid)
+                {
+                    if (Players.Count() < _maxPlayers)
+                    {
+                        deferrals.update(_language["ConnectingMessage"]);
+                        await Delay(_queueRefreshRate + 3250);
+                        deferrals.done();
+                    }
+                    else
+                    {
+                        QueuePlayer queuePlayer = new QueuePlayer(player, DateTime.UtcNow, false);
+                        waitingList.Add(queuePlayer);
+                        await UpdateQueuePlayer(player.Handle, deferrals);
+                    }
+                }
+                else
+                {
+                    deferrals.done(_language["ErrorMessage"]);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error during player connection: {ex.Message}");
+                deferrals.done($"An error occurred: {ex.Message}");
             }
 
             deferrals.done(_language["ErrorMessage"]);
+        }
+
+        private async Task UpdateQueuePlayer(string handle, dynamic deferrals)
+        {
+            while (true)
+            {
+                await Delay(1000);
+
+                var queuePlayer = waitingList.FirstOrDefault(p => p.Player.Handle == handle);
+
+                if (queuePlayer != null)
+                {
+                    if (queuePlayer.CanJoin)
+                    {
+                        deferrals.update(_language["ConnectingMessage"]);
+                        waitingList.Remove(queuePlayer);
+                        await Delay(_queueRefreshRate + 3250);
+                        deferrals.done();
+                        break;
+                    }
+                    else
+                    {
+                        // Calculate the player's position in the queue
+                        int positionInQueue = waitingList.IndexOf(queuePlayer) + 1;
+
+                        // Calculate the time in queue
+                        TimeSpan timeInQueue = DateTime.UtcNow - queuePlayer.JoinedOn;
+                        string timeInQueueFormatted = timeInQueue.ToString(@"mm\:ss");
+
+                        // Update the deferral message with the queue position and time
+                        string queueUpdateMessage = string.Format(_language["QueueUpdateMessage"], positionInQueue, timeInQueueFormatted);
+                        deferrals.update(queueUpdateMessage);
+                    }
+                } 
+                else
+                {
+                    break;
+                }
+                await Delay(0);
+
+
+            }
         }
 
         private Dictionary<string, string> GetUserIdentifiers(Player player)
@@ -135,7 +226,6 @@ namespace lx_connect.Server
 
             return false;
         }
-
 
         //TODO: call from lxEF
         public static string ConvertSteamIDHexToDec(string hexSteamID)
